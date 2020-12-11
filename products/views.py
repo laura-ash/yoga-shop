2f7c07535387b179ee208feec69822553538432d
laura-ash <lauraeashmore@gmail.com>
2020-12-11 20:00:12 +0000

    Added product views and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product 
 
 # Create your views here.
+
+def products(request):
+    """ View to show products """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)

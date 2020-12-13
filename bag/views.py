6f5f3d31db6ecc13a03443eb0d36367cfa926d8b
laura-ash <lauraeashmore@gmail.com>
2020-12-13 10:53:26 +0000

    Created and styled bag contents page

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_bag(request):
+    "View to show users products in their bag"
+
+    return render(request, 'bag/bag.html')

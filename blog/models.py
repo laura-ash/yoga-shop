c8850f3e990e8acbac4d95a06c5218df6337b954
laura-ash <lauraeashmore@gmail.com>
2021-01-07 12:57:38 +0000

    Created blo models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=120, null=True, blank=True)
+    slug = models.SlugField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    author = models.CharField(max_length=140, null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+

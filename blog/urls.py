35505799b3446d35ed52c52bac8147d0d0c9e866
laura-ash <lauraeashmore@gmail.com>
2021-01-09 12:43:44 +0000

    Added named urls for blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.blog_home),
+    url(r'^(?P<slug>[\w-]+)/$', views.blog_post, name="detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 def blog_home(request):
     posts = Post.objects.all().order_by('date')
     return render(request, 'blog/blog_home.html', {'posts': posts})
+
+def blog_post(request, slug):
+    return HttpResponse(slug)

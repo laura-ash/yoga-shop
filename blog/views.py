a32cb04d3181ed9639487d9f942b92a2fb9fd2f4
laura-ash <lauraeashmore@gmail.com>
2021-01-09 13:03:58 +0000

    Created individual blog post template

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,6 @@ def blog_home(request):
     return render(request, 'blog/blog_home.html', {'posts': posts})
 
 def blog_post(request, slug):
-    return HttpResponse(slug)
+    # return HttpResponse(slug)
+    post = Post.objects.get(slug=slug)
+    return render(request, 'blog/blog_post.html', {'post': post})

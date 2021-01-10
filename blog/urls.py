c2932c8d473940391076dbf43830800f21919248
laura-ash <lauraeashmore@gmail.com>
2021-01-10 15:45:21 +0000

    Created events template

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.blog_home),
+    url(r'^$',views.blog_home, name="blog_home"),
     url(r'^(?P<slug>[\w-]+)/$', views.blog_post, name="detail"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Event
 
-# Create your views here.
+def events(request):
+    events = Event.objects.all().order_by('date')
+    return render(request, 'events/events.html', {'events': events})

cab77247e0a7e325ce501278f8d56d22ea019b4c
laura-ash <lauraeashmore@gmail.com>
2021-01-12 17:32:17 +0000

    Added our space page and updated main nav

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.events, name='events')
+    path('', views.events, name='events'),
+    path('event-space', views.event_space, name='event_space')
 ]
--- events/views.py
+++ events/views.py
@@ -4,3 +4,6 @@ from .models import Event
 def events(request):
     events = Event.objects.all().order_by('date')
     return render(request, 'events/events.html', {'events': events})
+
+def event_space(request):
+    return render(request, 'events/event-space.html')

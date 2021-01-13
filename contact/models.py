75b894b3b5a079dc0e58081da7e578017e711f39
laura-ash <lauraeashmore@gmail.com>
2021-01-13 19:53:22 +0000

    Created models for contact section

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings 
+
+
+class Contact(models.Model):
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+

dfcf221cbc39f77eaf1eafe46019ddb6c10b0258
laura-ash <lauraeashmore@gmail.com>
2021-01-16 10:54:35 +0000

    Save form submit to database

--- contact/views.py
+++ contact/views.py
@@ -1,17 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
 from .models import Contact
 
 
 def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            #save submission to db`
+            form.save()
+            return redirect('events')
+    else:
 
-    contact_form = ContactForm()
+        contact_form = ContactForm()
 
-    template = 'contact/contact_us.html'
-    context = {
-        'contact_form': contact_form,
+        template = 'contact/contact_us.html'
+        context = {
+            'contact_form': contact_form,
 
-}
+        }
 
     return render(request, 'contact/contact_us.html', context)
 

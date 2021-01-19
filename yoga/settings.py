58adde26892731ba392cfa7e47b772b740f0ab9d
laura-ash <lauraeashmore@gmail.com>
2021-01-19 18:13:43 +0000

    Removed secret key and added debug conditions

--- yoga/settings.py
+++ yoga/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-ze%i=(nud7=7tsnb+)g(=wo+ow+*todfe(^+m_@t*)o$-ik$0'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['yoga-shop.herokuapp.com', 'localhost']
 

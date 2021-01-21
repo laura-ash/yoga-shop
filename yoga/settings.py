d295fb3e0f9e4ab29b9c0d21d2df20a64820f9a0
laura-ash <lauraeashmore@gmail.com>
2021-01-21 19:41:41 +0000

    Imported env.py to settings, removed empty class in html

--- yoga/settings.py
+++ yoga/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+if os.path.exists('env.py'):
+	import env
 import dj_database_url
 
 from pathlib import Path
@@ -24,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -48,7 +51,6 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-    'crispy_forms',
     'profiles',
     'blog',
     'events',

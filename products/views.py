7163a13ba6a011f92a7fda50e8f7c5b42df4b4f8
laura-ash <lauraeashmore@gmail.com>
2021-01-23 11:41:14 +0000

    Imported messages to product views

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, render, redirect, reverse
+from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
 

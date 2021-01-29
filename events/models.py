2cb1888b5dc8174175a43c69c065a2dd82c98851
laura-ash <lauraeashmore@gmail.com>
2021-01-29 13:15:50 +0000

    Changed datetime property to string in event model

--- events/models.py
+++ events/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Event(models.Model):
     name = models.CharField(max_length=120, null=True, blank=True)
     slug = models.SlugField(null=True, blank=True)
-    date = models.DateTimeField(null=True, blank=True)
+    date = models.CharField(max_length=200, null=True, blank=True)
     facilitator = models.CharField(max_length=140, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     level = models.CharField(max_length=80, null=True, blank=True)

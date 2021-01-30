bcff21bc639909987ee81998fd0908c337f8587d
laura-ash <lauraeashmore@gmail.com>
2021-01-30 15:06:22 +0000

    Added sweetify messages for contacts page

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ContactForm
 from .models import Contact
+import sweetify
 
 
 def contact(request):
@@ -8,7 +9,10 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            sweetify.success(request, 'Message successfully sent', position='top-right', toast='true', icon='success', timer= '3000',)
             return redirect('contact')
+        else:
+            sweetify.success(request, 'Unsuccessful. Please check each form field is valid!', position='top-right', toast='true', icon='success', timer= '3000',)
     else:
 
         contact_form = ContactForm()
@@ -35,6 +39,10 @@ def update_submission(request, pk):
         form = ContactForm(request.POST, instance=submission)
         if form.is_valid():
             form.save()
+            sweetify.success(request, 'Successfully updated.', position='top-right', toast='true', icon='success', timer= '3000',)
+        else: 
+            sweetify.success(request, 'Update unsuccessful. Please check all fields are valid and try again.', position='top-right', toast='true',
+            icon='error', timer= '3000',)
     return redirect(submissions)
 
 
@@ -42,5 +50,8 @@ def delete_submission(request, pk):
     submission = get_object_or_404(Contact, pk=pk)
     if request.method == "POST":
         submission.delete()
-
+        sweetify.success(request, 'Successfully deleted.', position='top-right', toast='true', icon='success', timer= '3000',)
+    else: 
+        sweetify.success(request, 'Deletion unsuccessful. Please try again.', position='top-right', toast='true',
+            icon='error', timer= '3000',)
     return redirect(submissions)

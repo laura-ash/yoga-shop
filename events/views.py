e8cc92c5d696f56d0fae59470516b0bdf0dd239d
laura-ash <lauraeashmore@gmail.com>
2021-01-30 15:17:19 +0000

    Added sweetify messages for events app

--- events/views.py
+++ events/views.py
@@ -3,6 +3,8 @@ from django. contrib import messages
 from .forms import EventForm
 from .models import Event
 
+import sweetify
+
 
 def events(request):
     events = Event.objects.all().order_by('date')
@@ -23,23 +25,17 @@ def create_event(request):
     if request.method == 'POST':
         event_form = EventForm(request.POST, request.FILES)
 
-        context = {
-            'event_form': event_form,
-        }
-
         if event_form.is_valid():
             event_form.save()
+            sweetify.success(request, 'Event created successfully.', position='top-right', toast='true',
+            icon='success', timer= '3000',)
             return redirect('events')
         else:
-        
             event_form = EventForm()
-
-        template = 'events/create-event.html'
+            sweetify.success(request, 'Event creation unsuccessful. Please ensure all fields are valid and try again',
+            position='top-right', toast='true', icon='error', timer= '3000',)
+            template = 'events/create-event.html'
         
-        context = {
-            'event_form': event_form,
-        }
-
     return render(request, 'events/create-event.html', context)
 
 
@@ -49,9 +45,13 @@ def update_event(request, pk):
         event_form = EventForm(request.POST, request.FILES, instance=event)
         if event_form.is_valid():
             event_form.save()
+            sweetify.success(request, 'Event updated successfully.', position='top-right', toast='true',
+            icon='success', timer= '3000',)
         else:
             print('form not valid')
             print(event_form)
+            sweetify.success(request, 'Event update unsuccessful. Please ensure all fields are valid and try again.',
+                position='top-right', toast='true', icon='error', timer= '3000',)
     return redirect(events)
 
 
@@ -59,4 +59,9 @@ def delete_event(request, pk):
     event = get_object_or_404(Event, pk=pk)
     if request.method == "POST":
         event.delete()
+        sweetify.success(request, 'Event deleted successfully.', position='top-right', toast='true',
+            icon='success', timer= '3000',)
+    else: 
+        sweetify.success(request, 'Event deletion unsuccessful. Please try again.',
+            position='top-right', toast='true', icon='error', timer= '3000',)
     return redirect(events)

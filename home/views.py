eef9730548638b07e74027ef7e784bcbfaeea20d
laura-ash <lauraeashmore@gmail.com>
2021-01-31 13:01:48 +0000

    Validated python in home app

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from blog.models import Post
 import sweetify
 
+
 def index(request):
     posts = Post.objects.all().order_by('date')[:3]
     return render(request, 'home/index.html', {'posts': posts})

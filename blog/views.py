5ee86767d4d9b3c996826de4eb3fb14a5b1f1262
laura-ash <lauraeashmore@gmail.com>
2021-01-31 12:43:58 +0000

    Validated python in blog app

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from .models import Post
 from django.http import HttpResponse
 
+
 def blog_home(request):
     posts = Post.objects.all().order_by('date')
     return render(request, 'blog/blog-home.html', {'posts': posts})
+
 
 def blog_post(request, slug):
     # return HttpResponse(slug)
